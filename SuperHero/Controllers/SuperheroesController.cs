4c03a2c871cf66cbe9a57e297482ea28d1b7b0d1
jorge bejarano <jorge.bejarano@yahoo.com>
2020-04-21 11:51:11 -0400

    continuing to work on controller

--- SuperHero/Controllers/SuperheroesController.cs
+++ SuperHero/Controllers/SuperheroesController.cs
@@ -4,15 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SuperHero.Data;
+using SuperHero.Models;
 
 namespace SuperHero.Controllers
 {
     public class SuperherosController : Controller
     {
         // GET: Superheros
+        private ApplicationDbContext db;
+
+        public Superheros SuperHeroes { get; private set; }
+
+        public SuperherosController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
         public ActionResult Index()
         {
-            return View();
+            var superheroes = db.Superheroes.ToList();
+            return View(superheroes);
         }
 
         // GET: Superheros/Details/5
@@ -22,8 +33,10 @@ namespace SuperHero.Controllers
         }
 
         // GET: Superheros/Create
-        public ActionResult Create()
+        public ActionResult Create(SuperherosController superHero)
         {
+            db.Superheroes.Add(SuperHeroes);
+            db.SaveChanges();
             return View();
         }
 
